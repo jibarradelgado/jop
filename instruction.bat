24add29756f88e8cd89d9aeae6b7d60b66ac9636
Jorge Ibarra <jorgeid9@gmail.com>
2014-04-30 17:49:06 +0200

    Change of instruction.bat

--- instruction.bat
+++ instruction.bat
@@ -1,20 +1,21 @@
-set param1=test/wcet/test/src
-set param2=test
-set param3=IfForLoops
-set param4=measure10000
+set param1=test
+set param2=wcet/mrtc
+set param3=SimultaneousLinearEquations
+set param4=run
 make java_app wcet -e P1="%param1%" P2="%param2%" P3="%param3%" WCET_METHOD="%param4%" WCET_DFA=yes
 
+set param2=%param2:/=_%
 
 if exist "java\target\wcet\%param2%_%param3%_%param4%\report" (
 	echo Directory exists
 	cd "java\target\wcet\%param2%_%param3%_%param4%\report"
 	make
 	index.html
+	pause
 	exit
 ) else (
 	echo There was an error with the path
 	pause
 	exit
 ) 
-
 
